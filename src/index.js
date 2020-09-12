74809cb37c1c66c8303f5d88686159998c70b947
Mykola <mykola@bleech.de>
2020-09-12 23:04:38 +0200

    feat(index.js): add interaction element, mouse over/click event, install TweenMax, add animation on circle click

--- src/index.js
+++ src/index.js
@@ -1,52 +1,75 @@
 import * as THREE from 'three/build/three.module';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { TweenMax } from 'gsap/gsap-core';
 
-let scene, camera, renderer, hlight, directionalLight, light, light2, controls;
+let scene, camera, renderer, hlight, directionalLight, light, light2, controls, circle, house;
 
 function init() {
-   scene = new THREE.Scene();
+  scene = new THREE.Scene();
   //  scene.background = new THREE.Color(0x000000);
-   camera = new THREE.PerspectiveCamera(40, window.innerWidth/window.innerHeight, 1, 5000);
-   camera.rotation.y = 45/180*Math.PI;
-   camera.position.x = 320;
-   camera.position.y = 200;
-   camera.position.z = 360;
+  camera = new THREE.PerspectiveCamera(40, window.innerWidth/window.innerHeight, 1, 5000);
+  camera.rotation.y = 45/180*Math.PI;
+  camera.position.x = 320;
+  camera.position.y = 200;
+  camera.position.z = 360;
 
-   renderer = new THREE.WebGLRenderer({antialias: true, alpha: true });
-   renderer.setClearColor( 0xffffff, 0);
-   renderer.setSize(window.innerWidth, window.innerHeight);
-   document.body.appendChild(renderer.domElement)
+  renderer = new THREE.WebGLRenderer({antialias: true, alpha: true });
+  renderer.setClearColor( 0xffffff, 0);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement)
 
-   controls = new OrbitControls(camera, renderer.domElement);
-   controls.autoRotate = true;
+  controls = new OrbitControls(camera, renderer.domElement);
+  // controls.autoRotate = true;
+  // ---------- limit rotation vertical
+  controls.minPolarAngle = 45/180*Math.PI;
+  controls.maxPolarAngle = Math.PI/2; // don't let to go below the ground
+  controls.minDistance = 300; // max zoom in
+  controls.maxDistance = 700; // max zoom out
+  // ---------- limit rotation horizontal
+  // controls.minAzimuthAngle = 0;
+  // controls.maxAzimuthAngle = Math.PI/2;
 
-   hlight = new THREE.AmbientLight(0x404040, 5);
-   scene.add(hlight);
+  hlight = new THREE.AmbientLight(0x404040, 5);
+  scene.add(hlight);
 
-   directionalLight = new THREE.DirectionalLight(0x404040, 5);
-   directionalLight.position.set(0,1,0);
-   directionalLight.castShadow = true;
-   scene.add(directionalLight);
+  directionalLight = new THREE.DirectionalLight(0x404040, 5);
+  directionalLight.position.set(0,1,0);
+  directionalLight.castShadow = true;
+  scene.add(directionalLight);
 
-   light = new THREE.PointLight(0x404040, 4);
-   light.position.set(0, -800, 0);
-   scene.add(light);
+  light = new THREE.PointLight(0x404040, 4);
+  light.position.set(0, -800, 0);
+  light.castShadow = true;
+  scene.add(light);
 
-   light2 = new THREE.PointLight(0x404040, 5);
-   light2.position.set(550, 1000, 550);
-   scene.add(light2);
+  light2 = new THREE.PointLight(0x404040, 5);
+  light2.position.set(550, 1000, 550);
+  light2.castShadow = true;
+  scene.add(light2);
 
-   let loader = new GLTFLoader();
-   loader.load('object/scene.gltf', (gltf) => {
-       gltf.scene.children[0].scale.set(1, 1, 1);
-       scene.add(gltf.scene);
-       animate();
-   });
+  // add circle dot
+  let geometry = new THREE.CircleGeometry( 8, 32 );
+  let material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+  circle = new THREE.Mesh( geometry, material );
+  scene.add( circle );
+  circle.position.set(42, 50, -35)
+
+  let pickableMeshes = [];
+  let loader = new GLTFLoader();
+  loader.load('object/scene.gltf', (gltf) => {
+      house = gltf.scene;
+      house.children[0].scale.set(1, 1, 1);
+      scene.add(house);
+      animate();
+  });
+
+  console.log(pickableMeshes)
 }
 
 function animate() {
     requestAnimationFrame(animate);
+    circle.rotation.y = Math.atan2( ( camera.position.x - circle.position.x ), ( camera.position.z - circle.position.z ) );
     controls.update();
     renderer.render(scene, camera);
 }
@@ -59,3 +82,72 @@ function updateCanvasSize() {
 
 init();
 window.addEventListener('resize', updateCanvasSize);
+
+let mouse = new THREE.Vector2(0,0);
+let raygun = new THREE.Raycaster();
+let useRaycast = true;
+let hits;
+
+// Raycast when mouse over
+function onMouseMove () {
+  watchMouse()
+  if (hits.length > 0) {
+      for (var i = 0; i < hits.length; i++ ) {
+        document.body.style.cursor = 'pointer';
+          // Change material color of item we clicked on
+          hits[i].object.material.color.set(0xff0000);
+      }
+      renderer.render(scene, camera);
+  } else {
+    document.body.style.cursor = '';
+    circle.material.color.set(0xffff00);
+  }
+}
+
+// Raycast when click
+function onClick () {
+    watchMouse()
+    if (hits[0]) {
+      // get the current camera position
+      const { x, y, z } = camera.position
+      const start = new THREE.Vector3(x, y, z)
+			const item = hits[0]
+
+      var point = item.point;
+      var camDistance = camera.position.length();
+      camera.position.copy(point).normalize().multiplyScalar(camDistance);
+      // controls.update();
+
+      // save the camera position
+      const { x: a, y: b, z: c } = camera.position
+
+      // invert back to original position
+      camera.position.copy(start).normalize().multiplyScalar(camDistance)
+
+      // animate from start to end
+      TweenMax.to(camera.position, 1, { x: a, y: b, z: c, onUpdate: () => {
+          controls.update()
+        }
+      });
+    }
+}
+
+function watchMouse () {
+  // Get mouse position in screen space
+  mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+  mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+  // Only raycast if not panning (optimization)
+  if (useRaycast) {
+      raygun.setFromCamera(mouse, camera);
+
+      // Raycast to single object
+      hits = raygun.intersectObject(circle, false);
+
+      // Raycast to multiple objects
+      // hits = raygun.intersectObjects([myTargetObect, myTargetObect2]);
+  }
+}
+
+window.addEventListener('mousemove', onMouseMove, false);
+window.addEventListener('click', onClick, false);
